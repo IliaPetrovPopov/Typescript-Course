a9dfc012f9dbd7923880d96b2f38c53d0581d31d
Ilia Popov <ilia_popov02@abv.bg>
2023-10-25 18:19:22 +0300

    Make Sync module more general

---
+++ framework/src/models/APISync.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosPromise } from "axios";
+
+interface HasID {
+  id?: number;
+}
+
+export class APISync<T extends HasID> {
+  constructor(private rootUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
+  }
+  // TS wants to know if <T> type will have id inside
+  // but it is not provided, so I explicitly set constrains to <T>
+  save(data: T): AxiosPromise {
+    const id = data.id;
+
+    if (id) {
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      return axios.post(this.rootUrl, data);
+    }
+  }
+}
