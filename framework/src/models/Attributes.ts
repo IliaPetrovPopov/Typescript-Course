9acdcfc59a773f14a4f6fab303c83c77b24a2f59
Ilia Popov <ilia_popov02@abv.bg>
2023-10-25 18:19:46 +0300

    Add getAllData method and remove unused code

--- framework/src/models/Attributes.ts
+++ framework/src/models/Attributes.ts
@@ -1,21 +1,19 @@
-export interface UserProps {
-  id?: number;
-  name?: string;
-  age?: number;
-}
-
-export class Attributes<T extends object > {
+export class Attributes<T extends object> {
   private userData: T;
 
   constructor(userData: T) {
     this.userData = userData;
   }
 
-  get<K extends keyof T>(key: K): T[K] {
+  get = <K extends keyof T>(key: K): T[K] => {
     return this.userData[key];
   }
 
   set(updateProps: T): void {
     Object.assign(this.userData, updateProps);
+  }
+
+  getAllData(): T {
+    return this.userData;
   }
 }
