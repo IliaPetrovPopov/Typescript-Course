1eda990338d824d5763636da85199e3e90fffd4c
Ilia Popov <ilia_popov02@abv.bg>
2023-10-25 18:21:24 +0300

    Modify User class to inherit Model parent class

--- framework/src/models/User.ts
+++ framework/src/models/User.ts
@@ -1,5 +1,8 @@
+import { APISync } from "./APISync";
+import { Attributes } from "./Attributes";
+import { Collection } from "./Collection";
 import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
+import { Model } from "./Model";
 
 export interface UserProps {
   id?: number;
@@ -7,11 +10,25 @@ export interface UserProps {
   age?: number;
 }
 
-type UserPropTypes = number | string;
-
 const rootUrl = "http://localhost:3000/users";
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs), 
+      new Eventing(),
+      new APISync<UserProps>(rootUrl)
+      );
+  }
+
+  static buildCollection(): Collection<User, UserProps> {
+    return new Collection <User, UserProps>
+    (
+        rootUrl,
+        (json: UserProps) => User.buildUser(json)
+    );
+  } 
 }
+
+const user = User.buildUser({});
+user.get("name")
