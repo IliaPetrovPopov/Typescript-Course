97ca095d74fc1b35dbb94898bcc27778681a8cac
Ilia Popov <ilia_popov02@abv.bg>
2023-10-26 17:57:47 +0300

    Modify UserForm & test code in index.ts

--- framework/src/index.ts
+++ framework/src/index.ts
@@ -1,5 +1,18 @@
 import { User } from "./models/User";
+import { UserEdit } from "./views/UserEdit";
+import { UserForm } from "./views/UserForm";
 
-const user = new User({ id: 1 });
+const rootElement = document.querySelector("#root") as HTMLElement;
 
-user.fetch();
+const user = User.buildUser({ age: 18, name: "Anthony" });
+
+if(rootElement) {
+    const userEdit = new UserEdit(rootElement, user);
+
+    userEdit.render();
+
+    console.log(userEdit.regions);
+    
+}
+
+// userForm.render();
--- framework/src/views/UserForm.ts
+++ framework/src/views/UserForm.ts
@@ -1,19 +1,41 @@
-export class UserForm {
- constructor(public parent: Element) {};
+import { User, UserProps } from "../models/User";
+import { View } from "./View";
+
+export class UserForm extends View<User, UserProps> {
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      "click:#set-age": this.onSetAge,
+      "click:#change-name": this.onSetName,
+      "click:#save-user": this.onSave,
+    };
+  }
+
+  onSetAge = (): void => {
+    this.model.setRandomAge();
+  };
+
+  onSetName = (): void => {
+    const input = this.parent.querySelector("input");
+
+    const name = input?.value;
+
+    if (name) {
+      this.model.set({ name });
+    }
+  };
+
+  onSave = (): void => {
+    this.model.save();
+  };
 
   template(): string {
     return `
         <div>
-            <h1>User Form</h1>
-            <input />
+            <input placeholder="${this.model.get("name")}" />
+            <button id="change-name"> Change Name </button>
+            <button id="set-age"> Set Random Age </button>
+            <button id="save-user"> Save </button>
         </div>
         `;
-  }
-
-  render(): void {
-    const templateElement = document.createElement("template");
-    templateElement.innerHTML = this.template();
-
-    this.parent.appendChild(templateElement.content);
   }
 }
